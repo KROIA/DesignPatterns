5984149a44316476f3ff67976c29ced181dfb0df
Alex Krieg <alexkrieg@gmx.ch>
2023-05-31 14:10:41 +0200

    -

--- UnitTests/Tests/TestSingelton.h
+++ UnitTests/Tests/TestSingelton.h
@@ -26,10 +26,7 @@ private:
 class Singelton_1 : public Utilities::Singleton<Singelton_1> // Uses the classname as template param
 {
 public:
-	const std::string& getName()
-	{
-		return m_name;
-	}
+
 private:
 	// Needs this class as friend to be able to create a instance of this class
 	friend Singleton<Singelton_1>;
@@ -37,35 +34,28 @@ private:
 	Singelton_1()
 		: Singleton<Singelton_1>()
 	{
-		m_name = "Singelton_1";
+		
 	}
-
-	// Some data inside the Singelton
-	std::string m_name;
 };
 class Singelton_2 : public Utilities::Singleton<Singelton_2>
 {
 public:
-	static std::string getName()
-	{
-		return getInstance().m_className;
-	}
+	
 private:
 	friend Singleton<Singelton_2>;
 	Singelton_2()
 		: Singleton<Singelton_2>()
 	{
-		m_name = "Singelton_2";
+
 	}
-	std::string m_name;
 };
 
 
 bool TestSingelton::test_access()
 {
 	Singelton_1& instance = Singelton_1::getInstance();
-	printLn("Singelton_1.getName(): " + instance.getName());
-	printLn("Singelton_2.getName(): " + Singelton_2::getName());
+	printLn(std::string("Singelton_1.getName(): ") + instance.getName());
+	printLn(std::string("Singelton_2.getName(): ") + Singelton_2::getName());
 
 	ASSERT_TRUE(std::string(instance.getName()).size() == 0, "");
 	ASSERT_TRUE(std::string(Singelton_2::getName()).size() == 0, "");
--- core/include/Singelton.h
+++ core/include/Singelton.h
@@ -14,9 +14,25 @@ namespace Utilities
          *  @brief Returns the instance of the singleton object.
          *  @return The instance of the singleton object.
          */
-        static T& getInstance() {
-            static T instance;
-            return instance;
+        static T& getInstance()
+        {
+            if (!m_instance)
+            {
+                m_instance = new T();
+            }
+            return *m_instance;
+        }
+        static void destroy()
+        {
+            delete m_instance;
+        }
+        static const char *getName()
+        {
+            if (!m_instance)
+            {
+                m_instance = new T();
+            }
+            return m_className;
         }
 
         /**
@@ -29,23 +45,39 @@ namespace Utilities
          */
         Singleton& operator=(const Singleton&) = delete;
 
+        
+
     protected:
         /**
          *  @brief Constructs a Singleton object.
          */
         Singleton()
-            : m_className(typeid(T).name())
+            //: m_instance(this)
+            //, m_className(typeid(T).name())
         {
-
+           // m_instance = this;
+            std::string name(typeid(T).name());
+            m_className = new char[name.size()+1];
+            strcpy(m_className, name.c_str());
         }
 
         /**
          *  @brief Destroys the Singleton object.
          */
-        ~Singleton() = default;
+        ~Singleton()
+        {
+            m_instance = nullptr;
+        }
 
-        const char* m_className; /**< The name of the class of the singleton object. */
+        static T* m_instance;
+        static char* m_className; /**< The name of the class of the singleton object. */
     };
+
+    template<class T>
+    T* Singleton<T>::m_instance = nullptr;
+
+    template<class T>
+    char* Singleton<T>::m_className = nullptr;
 
 
 }
